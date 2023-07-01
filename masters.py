13aaae91bfa369cf923db8376daba3dcb4787e95
Sheyla Leyva Sánchez <70074598+sheyls@users.noreply.github.com>
2023-07-01 15:40:26 -0400

    add process for task publishing and subscribing

--- masters.py
+++ masters.py
@@ -1,5 +1,7 @@
 import re
+import time
 from multiprocessing import Process, Queue
+from threading import Lock, Thread
 
 import zmq
 
@@ -9,6 +11,8 @@ from service_discovery import discover_peer, get_masters
 from sockets import CloudPickleContext, CloudPickleSocket, no_block_REQ
 
 log = get_logger("Master")
+
+lock_subscribers = Lock()
 
 
 def get_remote_tasks(master, tasks_queue, signkey=None):
@@ -39,6 +43,180 @@ def get_remote_tasks(master, tasks_queue, signkey=None):
             log.error(e, "Get Remote Tasks")
     sock.close()
     tasks_queue.put(None)
+
+
+def push_task(push_queue, addr, signkey=None):
+    """
+    Process that push tasks to workers and notify pulled tasks.
+    """
+    context = CloudPickleContext()
+    sock: CloudPickleSocket = context.socket(zmq.PUSH)
+    sock.bind(f"tcp://{addr}")
+
+    for url in iter(push_queue.get, messages.STOP):
+        log.debug(f"Pushing {url}", "Task Pusher")
+        sock.send_data(url.encode(), signkey=signkey)
+
+
+def task_publisher(addr, task_queue, signkey=None):
+    """
+    Process that publish tasks changes to others master nodes.
+    """
+    context = CloudPickleContext()
+    sock: CloudPickleSocket = context.socket(zmq.PUB)
+    sock.bind(f"tcp://{addr}")
+
+    while True:
+        try:
+            # task: (flag, url, data)
+            task = task_queue.get()
+            if isinstance(task[0], bool):
+                if isinstance(task[2][1], int):
+                    log.debug(
+                        f"Publish pulled task: ({task[0]}, {task[1]})", "Task Publisher"
+                    )
+                    sock.send_data([messages.PULLED_TASK, task], signkey=signkey)
+                    continue
+
+                header = task[2][0]
+                log.debug(f"Publish {header} of {task[1]}", "Task Publisher")
+                sock.send_data([header.encode(), (task[1], task[2][1])])
+
+            elif task[0] == messages.PURGE:
+                log.debug(f"Publish PURGE", "Task Publisher")
+                sock.send_data([messages.PURGE, messages.JUNK], signkey=signkey)
+
+            elif task[0] == messages.REMOVE:
+                log.debug(f"Publish REMOVE", "Task Publisher")
+                sock.send_data([messages.REMOVE, task[1]], signkey=signkey)
+
+            else:
+                log.debug(f"Publish master: ({task[0]}:{task[1]})", "Task Publisher")
+                sock.send_data([messages.NEW_MASTER, task], signkey=signkey)
+
+        except Exception as e:
+            log.error(e, "Task Publisher")
+
+
+def connect_to_publishers(sock, peer_queue):
+    """
+    Thread that connect subscriber socket to masters.
+    """
+    for addr, port in iter(peer_queue.get, messages.STOP):
+        with lock_subscribers:
+            log.debug(
+                f"Connecting to master {addr}:{port + 3}", "Connect to Publishers"
+            )
+            sock.connect(f"tcp://{addr}:{port + 3}")
+
+
+def disconnect_from_publishers(sock, peer_queue):
+    """
+    Thread that disconnect subscriber socket from masters.
+    """
+    for addr, port in iter(peer_queue.get, "STOP"):
+        with lock_subscribers:
+            log.debug(
+                f"Disconnecting from master {addr}:{port + 3}",
+                "Disconnect from Publishers",
+            )
+            sock.disconnect(f"tcp://{addr}:{port + 3}")
+
+
+def task_subscriber(
+    peer_queue,
+    disconnect_queue,
+    task_queue,
+    master_queue,
+    data_queue,
+    purge_queue,
+    signkey=None,
+):
+    """
+    Process that subscribe to published tasks
+    """
+    context = CloudPickleContext()
+    sock: CloudPickleSocket = context.socket(zmq.SUB)
+    sock.setsockopt(zmq.SUBSCRIBE, messages.PULLED_TASK)
+    sock.setsockopt(zmq.SUBSCRIBE, messages.NEW_MASTER)
+    sock.setsockopt(zmq.SUBSCRIBE, messages.UPDATE)
+    sock.setsockopt(zmq.SUBSCRIBE, messages.NEW_DATA)
+    sock.setsockopt(zmq.SUBSCRIBE, messages.FORCED)
+    sock.setsockopt(zmq.SUBSCRIBE, messages.REMOVE)
+    sock.setsockopt(zmq.SUBSCRIBE, messages.REMOVE)
+
+    connect_thread = Thread(
+        target=connect_to_publishers,
+        name="Connect to Publishers",
+        args=(sock, peer_queue),
+    )
+    connect_thread.start()
+
+    # disconnect_thread = Thread(target=disconnect_from_publishers, name="Disconnect from Publishers", args=(sock, disconnect_queue))
+    # disconnect_thread.start()
+
+    time.sleep(1)
+
+    while True:
+        try:
+            with lock_subscribers:
+                header, task = sock.recv_data(signkey=signkey)
+
+                log.info(f"Received Subscribed message: {header}", "Task Subscriber")
+                if header == messages.PULLED_TASK:
+                    # task: (flag, url, data)
+                    flag, url, data = task
+                    task_queue.put((flag, url, data))
+                elif header == messages.APPEND:
+                    # task: (address, port)
+                    addr, port = task
+                    master_queue.put((messages.APPEND, (addr, port)))
+                    peer_queue.put((addr, port))
+                elif header == messages.REMOVE:
+                    # task: (address, port)
+                    addr, port = task
+                    master_queue.put((messages.REMOVE, (addr, port)))
+                    # disconnectQ.put((addr, port))
+                elif header == "PURGE":
+                    purge_queue.put(False)
+                else:
+                    # header: UPDATE, NEW_DATA, FORCED
+                    # task: (url, data)
+                    data_queue.put((header, task))
+        except Exception as e:
+            log.error(e, "Task Subscriber")
+
+
+def worker_attender(pulled_queue, result_queue, failed_queue, addr, signkey=None):
+    """
+    Process that listen notifications from workers.
+    """
+    context = CloudPickleContext()
+    sock: CloudPickleSocket = context.socket(zmq.REP)
+    sock.bind(f"tcp://{addr}")
+
+    while True:
+        try:
+            msg = sock.recv_data(signkey=signkey)
+            if msg[0] == messages.PULLED:
+                log.info(f"Message received: {msg}", "Worker Attender")
+                # msg = PULLED, url, workerAddr
+                pulled_queue.put((False, msg[1], msg[2]))
+            elif msg[0] == messages.DONE:
+                log.info(f"Message received: ({msg[0]}, {msg[1]})", "Worker Attender")
+                # msg = DONE, url, html
+                result_queue.put((True, msg[1], msg[2]))
+            elif msg[0] == messages.FAILED:
+                log.info(f"Message received: {msg}", "Worker Attender")
+                # msg = FAILED, url, timesAttempted
+                failed_queue.put((False, msg[1], msg[1]))
+
+            # nothing important to send
+            sock.send_data(messages.OK, signkey=signkey)
+        except Exception as e:
+            # Handle connection error
+            log.error(e, "Worker Attender")
+            continue
 
 
 class MasterNode:
